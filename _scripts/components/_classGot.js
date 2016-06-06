e3abdf5a63ef86f0705729bd2d1d6131e6f79794
Kevin Albessard <kevin.albessard@scmfrance.fr>
2016-06-06 15:20:01 +0200

    More GOT stuffs

--- _scripts/components/_classGot.js
+++ _scripts/components/_classGot.js
@@ -17,12 +17,14 @@ class GOT {
 
 const throne = new GOT();
 let got_kingdom = document.querySelectorAll('.got_kingdom');
+let got_layer = document.querySelectorAll('.got_layer');
 let got_towers = document.querySelectorAll('.got_tower');
 let got_arrows = document.querySelectorAll('.got_arrow');
 let got_parliament_roof = document.querySelectorAll('.got_parliament_roof');
 
 
 throne.build(got_kingdom, 0);
-throne.build(got_towers, 10000);
-throne.build(got_arrows, 5000);
-throne.build(got_parliament_roof, 5000);
+throne.build(got_layer, 5000);
+throne.build(got_towers, 15000);
+throne.build(got_arrows, 10000);
+throne.build(got_parliament_roof, 10000);
